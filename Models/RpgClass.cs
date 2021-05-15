431fbe6b9e9c9c34617646e1ded27ff7a5674870
cloverfeng <clover.feng@tpisoftware.com>
2021-05-15 15:40:51 +0800

    Character 、 RpgClass 建立

---
+++ Models/RpgClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practice.Models
+{
+    public enum RpgClass
+    {
+        /// <summary>
+        /// 騎士
+        /// </summary>
+        Knight,
+        /// <summary>
+        /// 法師
+        /// </summary>
+        Mage,
+        /// <summary>
+        /// 牧師
+        /// </summary>
+        Cleric
+    }
+}
